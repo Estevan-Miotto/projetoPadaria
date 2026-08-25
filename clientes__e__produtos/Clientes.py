35bee914ad36ffc15580714bdf5b11a689e303fc
rodrigo <1111500@atitus.edu.br>
2026-08-25 11:32:20 -0300

    modificado a pasta cliente e clientes

--- clientes__e__produtos/Clientes.py
+++ clientes__e__produtos/Clientes.py
@@ -1,22 +1,21 @@
-from Cliente import Cliente
-from projetoPadaria.projeto_exemplo.models import cliente
+class Cliente:
+    def __init__(self, id, nome):
+        self.id = id
+        self.nome = nome
 
 class Clientes:
-
     def __init__(self):
         self.clientes = {}
         self.proximo_id = 1
 
     def cadastrar(self, nome):
         cliente = Cliente(self.proximo_id, nome)
-
         self.clientes[self.proximo_id] = cliente
-
         self.proximo_id += 1
 
     def listar_clientes(self):
         for cliente in self.clientes.values():
-            print(f"ID: {cliente.id} | "f"Nome: {cliente.nome}")
+            print(f"ID: {cliente.id} | Nome: {cliente.nome}")
 
     def buscar_cliente(self, codigo):
         return self.clientes.get(codigo)
